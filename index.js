70e91ac86353a729fcf021cdf0c28ff9342fdc23
Rafa Granados <rgranados@grupopremiere.com>
2017-10-11 12:54:09 -0600

    add login view

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // (C) Copyright 2017 Grupo Premiere 360
-
 const express = require('express');
 
 var app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hola GP360!');
+    res.render('login');
 });
 
 app.listen(port, () => {
-    console.log(`Started up at port ${port}`);
+    console.log(`Started up at port localhost:${port}`);
 });
